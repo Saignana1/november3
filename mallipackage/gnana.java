756ff635ebca45ea631d1e8a20d278b5871e7a66
malli <nagamalleswari.ece1@gmail.com>
2023-11-03 01:30:04 -0400

    change 1

--- mallipackage/gnana.java
+++ mallipackage/gnana.java
@@ -2,17 +2,12 @@ package mallipackage;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class gnana {
 	@Test
 	public void open() {
-		System.out.println("welcome to git");
+		System.out.println("Welcome to GIT");
 
 	}
 	}
